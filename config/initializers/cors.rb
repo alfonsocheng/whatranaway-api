d7b94b3a5c8fc5b5657bf5f887ab19638c463bac
Alfonso Cheng <acheng@liveramp.com>
2017-08-06 16:08:30 -0700

    fixing cors?

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
   allow do
     # origins 'http://localhost:3000'
-    origins 'https://www.whatranaway.com'
+    origins 'http://www.whatranaway.com'
     resource '*',
       headers: :any,
       methods: %i(get)# post put patch delete options head)
